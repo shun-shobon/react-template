586a38d0baba6e44bb36436212bc4ec4dbae2ccc
shun-shobon <me@shun.technology>
2020-09-25 18:40:51 +0900

    feat(webpack): add typescript

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,18 +2,36 @@ import * as webpack from "webpack";
 import * as path from "path";
 
 const isProduction = process.env.NODE_ENV === "production";
+const isDevelopment = !isProduction;
 
 const baseUrl = process.env.BASE_URL ?? "/";
 
 const config: webpack.Configuration = {
   mode: isProduction ? "production" : "development",
   entry: {
-    main: path.join(__dirname, "src", "index.js"),
+    main: path.join(__dirname, "src", "index.tsx"),
   },
   output: {
     publicPath: baseUrl,
     path: path.join(__dirname, "dist"),
     filename: "scripts/[name].[contenthash:8].js",
+  },
+  devtool: isDevelopment ? "inline-source-map" : false,
+  resolve: {
+    extensions: [".tsx", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+      }
+    ],
   },
 };
 
