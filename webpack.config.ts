76b4682632638af19d3b817067c004f7785500d0
shun-shobon <me@shun.technology>
2020-09-26 12:19:46 +0900

    feat(webpack): add copy plugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,6 +3,7 @@ import * as path from "path";
 import sass from "sass";
 import fibers from "fibers";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import CopyWebpackPlugin from "copy-webpack-plugin";
 
 const isProduction = process.env.NODE_ENV === "production";
 const isDevelopment = !isProduction;
@@ -101,6 +102,16 @@ const config: webpack.Configuration = {
       minify: isProduction,
       template: path.join(__dirname, "public", "index.html"),
       scriptLoading: "defer",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.join(__dirname, "public"),
+          globOptions: {
+            ignore: ["index.html"],
+          },
+        },
+      ],
     }),
   ],
 };
