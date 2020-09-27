9417b8b01e50b592370bb2224de98a33e6321d5b
shun-shobon <me@shun.technology>
2020-09-28 02:04:52 +0900

    feat: add sample

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+import styles from "./App.module.scss";
+
+const App: FC = () => (
+  <div>
+    <h1 className={styles.title}>Hello, world!</h1>
+  </div>
+);
+
+export default App;
