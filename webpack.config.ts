ec525487e057c86b5f660e850e46036cdcc730dd
Shuntaro Nishizawa <me@shun.technology>
2021-03-09 09:04:12 +0900

    feat: add babel-loader

--- webpack.config.ts
+++ webpack.config.ts
@@ -29,6 +29,7 @@ const config: Configuration = {
         test: /\.tsx?$/,
         exclude: /node_modules/,
         use: [
+          { loader: "babel-loader" },
           {
             loader: "ts-loader",
             options: {
