2c9d8498baae0312356f0992d47a4fa005db5fdb
Shuntaro Nishizawa <me@shun.technology>
2021-03-09 09:23:33 +0900

    feat: add react

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import type { FC } from "react";
+
+const App: FC = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,5 @@
-export {};
+import { render } from "react-dom";
+
+import App from "./App";
+
+render(<App />, document.getElementById("root"));
