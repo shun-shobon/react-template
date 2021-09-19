79af3c88d1a78358507da9ee5fb6670695be915c
Shuntaro Nishizawa <me@shun.technology>
2021-09-19 12:43:00 +0900

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+  },
+};
