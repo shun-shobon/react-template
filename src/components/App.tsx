69e73a2353e2e953d7066f41ee33f04e7efd7d23
Shuntaro Nishizawa <me@shun.technology>
2021-09-19 13:11:38 +0900

    feat: add type declares

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,10 @@
 import type { VFC } from "react";
 
+import styles from "./App.module.scss";
+
 const App: VFC = () => (
   <div>
-    <h1>Hello, React!</h1>
+    <h1 className={styles["title"]}>Hello, React!</h1>
   </div>
 );
 
